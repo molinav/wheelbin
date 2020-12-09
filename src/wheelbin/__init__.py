4d840e4228296093ee3acc12fafa3f99a16f472f
Víctor Molina García <molinav@users.noreply.github.com>
2020-12-09 09:13:47 +0100

    Set next development cycle 1.3.0+dev

--- src/wheelbin/__init__.py
+++ src/wheelbin/__init__.py
@@ -24,5 +24,5 @@
 #
 """wheelbin -- Compile all Python files inside a wheel to bytecode files."""
 
-__version__ = "1.3.0"
+__version__ = "1.3.0+dev"
 __author__ = "Grant Patten <grant@gpatten.com>"
